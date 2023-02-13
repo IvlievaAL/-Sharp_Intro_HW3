b6204f6ac7006c8a7249773b072c42b26a34731b
AlexIv <ivlieva.medphyslab.com>
2023-02-13 14:37:05 +0300

    Input of xyz added.Formula how to do is found and described in comment

--- Zadacha21/Zadacha21.cs
+++ Zadacha21/Zadacha21.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// задача21. на вход - декартовы координаты двух точек в объеме (по трем осям то есть).
+// Рассчитать расстояние между точками в объеме. 
+//Это та же теорема Пифагора, только к сумме квадратов катетов прибавляется третье слагаемое.
+System.Console.WriteLine("Enter number x1");
+int x1=int.Parse(Console.ReadLine());
+System.Console.WriteLine("Enter number y1");
+int y1=int.Parse(Console.ReadLine());
+System.Console.WriteLine("Enter number z1");
+int z1=int.Parse(Console.ReadLine());
+System.Console.WriteLine("Enter number x2");
+int x1=int.Parse(Console.ReadLine());
+System.Console.WriteLine("Enter number y2");
+int y1=int.Parse(Console.ReadLine());
+System.Console.WriteLine("Enter number z2");
+int z1=int.Parse(Console.ReadLine());
