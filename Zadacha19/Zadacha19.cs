2c755ecc8482004500245ea4d80730817f11404f
AlexIv <ivlieva.medphyslab.com>
2023-02-13 15:30:50 +0300

    Code for counting digits added as a base

--- Zadacha19/Zadacha19.cs
+++ Zadacha19/Zadacha19.cs
@@ -1,1 +1,33 @@
 ﻿// задача19. на вход - 5значное число. Проверить, является ли оно палиндромом.
+System.Console.WriteLine("Enter number: ");
+string StrN = System.Console.ReadLine();
+int number = Convert.ToInt32(StrN);
+int absnumber=Math.Abs(number);
+int Ndigits=1;
+int Ndivided=(absnumber/10);
+while (Ndivided>=1)
+   {
+      Ndigits++;
+      Ndivided=Ndivided/10;
+   }
+int[] arrayN=new int[Ndigits];
+int index=Ndigits-1;
+Ndivided=Ndivided+(absnumber/10);
+int Ndivided1=absnumber%10;
+while (index>=0)
+   {
+      arrayN[index]=Ndivided1;
+      Ndivided1=Ndivided%10;
+      Ndivided=Ndivided/10;
+      index=index-1;
+   }
+if (Ndigits<3)
+  {
+    System.Console.WriteLine("No third digit");
+  }
+else 
+  {
+    System.Console.WriteLine(arrayN[2]);
+  }
+   //var str = string.Join(" , ", arrayN);
+   //Console.WriteLine(str); полезная комбинация, выводит в консоль весь массив
